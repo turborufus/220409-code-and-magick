3c7d62b7acb853678027028be73d66ac0338894a
Alexandra Samonenko <turborufus@gmail.com>
2018-09-16 16:53:39 +0300

    Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -4,10 +4,11 @@ var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К�
 var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var WIZARDS_AMOUNT = 4;
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var getRandomInt = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -69,6 +70,100 @@ var createWizardElement = function (wizardTemplate, wizardObject) {
   return wizardElement;
 };
 
+var updateColorOfElement = function (wizardElement, inputName, colors) {
+  var selector = '[name="' + inputName + '"]';
+  var inputField = setupPlayer.querySelector(selector);
+
+  var newColor = colors[getRandomInt(0, colors.length - 1)];
+  if (wizardElement.classList.contains('setup-fireball')) {
+    wizardElement.parentElement.style.backgroundColor = newColor;
+  } else {
+    wizardElement.style.fill = newColor;
+  }
+
+  inputField.value = newColor;
+};
+
+var onPopupEscPress = function (evt) {
+  var userNameInput = setup.querySelector('.setup-user-name');
+  if ((evt.keyCode === ESC_KEYCODE) && (evt.target !== userNameInput)) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+var userNameInput = setup.querySelector('.setup-user-name');
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+var setupPlayer = setup.querySelector('.setup-player');
+var wizardCoat = setup.querySelector('.wizard-coat');
+var wizardEyes = setup.querySelector('.wizard-eyes');
+var wizardFireball = setup.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function (evt) {
+  updateColorOfElement(evt.target, 'coat-color', COAT_COLORS);
+});
+
+wizardEyes.addEventListener('click', function (evt) {
+  updateColorOfElement(evt.target, 'eyes-color', EYES_COLORS);
+});
+
+wizardFireball.addEventListener('click', function (evt) {
+  updateColorOfElement(evt.target, 'fireball-color', FIREBALL_COLORS);
+});
+
+var setupSubmit = setup.querySelector('.setup-submit');
+setupSubmit.addEventListener('click', function () {
+  closePopup();
+});
+
+setupSubmit.addEventListener('keydown', function () {
+  closePopup();
+});
+
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
@@ -81,4 +176,4 @@ for (var i = 0; i < WIZARDS_AMOUNT; i++) {
 }
 
 similarListElement.appendChild(fragment);
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
